4c91d384be160e47a029bf7c4a841c15db54f20d
Kanjtansu <kanjtansu@gmail.com>
2025-03-25 16:39:01 +0700

    [ADD] E2E testing for products

--- multilingual-product/test/app.e2e-spec.ts
+++ multilingual-product/test/app.e2e-spec.ts
@@ -1,25 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import TestAgent from 'supertest/lib/agent';
+import { AppModule } from '../src/app.module';
+import { LanguageCode } from '../src/products/enums/language.enum';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+    let app: INestApplication<App>;
+    let testAgent: TestAgent;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+        app = moduleFixture.createNestApplication();
+        await app.init();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+        if (app) testAgent = request(app.getHttpServer());
+    });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+    describe('/products', () => {
+        it('/products (POST)', () => {
+            return testAgent
+                .post('/products')
+                .send({
+                    sku: 'SKU-99999',
+                    translations: [
+                        {
+                            languageCode: LanguageCode.EN,
+                            name: 'Test Product',
+                            description: 'Just a test',
+                        },
+                        {
+                            languageCode: LanguageCode.TH,
+                            name: 'สินค้าเทสต์',
+                            description: 'รายละเอียด',
+                        },
+                    ],
+                })
+                .expect(201);
+        });
+
+        it('/products (GET)', () => {
+            return testAgent.get('/products?limit=20&page=1').expect(200);
+        });
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
 });
